cd81eddff4edef54d1d1a1b4a27650c7348df231
exl1pse <76978757+exllcc@users.noreply.github.com>
2021-08-13 22:24:32 +0300

    Delete js directory

--- js/preloader.js
+++
@@ -1,4 +0,0 @@
-$(window).on('load', function() { // makes sure the whole site is loaded 
-  $('#status').fadeOut(2950); // will first fade out the loading animation 
-  $('#preloader').delay(550).fadeOut('medium'); // will fade out the white DIV that covers the website. 
-})
